ffc797476a043421b737b6a76eaa2c5a32b7c8c6
lalbero <lucasalbero96@gmail.com>
2020-06-25 21:09:24 -0300

    Updated JQuery
    Now using Scryfall API (faster)
    Search being made when hitting enter
    after search the focus return to the text area

--- jquery.js
+++ jquery.js
@@ -1,31 +1,27 @@
 $(document).ready(function () {
     var count = 0;
+	$("input#card").keypress(function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            $("#btn_search").click();
+        }
+    });
     $("#btn_search").click(function(e) {
         var card = jQuery("input#card").val();
         //console.log(encodeURIComponent(card));
         $.ajax({
-            url: "https://api.magicthegathering.io/v1/cards?name="+encodeURIComponent(card)+"&language=portuguese%20(Brazil)",
+            url: "https://api.scryfall.com/cards/named?fuzzy="+encodeURIComponent(card),
             success: function(data){
-                console.log(data);
-                var found = false;
-                if(data.cards.length != 0){
-                    for(var i = 0; i < data.cards.length; i++){
-                        for(var j = 0; j < data.cards[i].foreignNames.length; j++) {
-                            if(data.cards[i].foreignNames[j].name.toUpperCase() == card.toUpperCase() && data.cards[i].imageUrl != null){
-                                $("#cardslist").append("<div id='carta_"+count+"'>"+data.cards[i].name+"</div><img id='cardImg' src='"+ data.cards[i].imageUrl +"'>");
-                                count++;
-                                $("input#card").val("");
-                                return false;
-                            }
-                        }
-                    }
-                    $("#cardslist").append("<div id='carta_"+count+"'>"+data.cards[0].name+"</div><img id='cardImg' src='"+ data.cards[0].imageUrl +"'>");
-                    count++;
-                    $("input#card").val("");
-                }else{
-                    alert("not found");
-                }
-            }
+                console.log(data); 
+                $("#cardslist").append("<div id='carta_"+count+"'>"+data.name+"</div><img id='cardImg' src='"+ data.image_uris.small +"'>");
+                count++;
+                $("input#card").val("");
+                $("input#card").focus();
+            },
+			error: function(data){
+				console.log(data);
+				alert("Error: "+data.responseJSON.details);
+			}
         });
     });
 });
